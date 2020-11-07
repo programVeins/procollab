b4bf883f7c10bb52db514569168a8cf7f75e7914
Sabesh Bharathi <sabesh5601@gmail.com>
2020-11-07 22:39:30 +0530

    Added missed files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # procollab
+
+### A place where college students/faculty can post people needed for projects with a specfic skillset and to search for projects to undertake
+
+## Installation
+
+* Frontend
+
+    Install node modules with `npm install`. Follow up with `npm start` to run the frontend at port 3000
+
+* Backend
+
+    Create a virtual environment using `python3 -m venv venv` and start it with `source venv/bin/activate`. Proceed to install all dependencies with `pip3 install -r requirements.txt`
+
+    Migration Scripts are run by these commands:
+    1. `flask db init`
+    2. `flask db migrate -m "message"`
+    3. `flask db upgrade`
+
+    This makes sure that the data models are converted to the schema and store in the sqlite database
+
+    Finally, start server with `flask run` on port 5000
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,35 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
-import { Button, Toast, ToastBody, ToastHeader} from 'reactstrap'
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Toast, ToastBody, ToastHeader} from 'reactstrap'
+import axios from 'axios';
+
+const backEndUrl = "http://localhost:5000"
 
 export default function Dashboard(props) {
 
-    const [redirect, setRedirect] = useState(false)
-
-    const projects = [
-        {
-            id: 1,
-            name: "Diabolical",
-            position: "Frontend developer",
-            createdUser: "Meganathan"
-        },
-        {
-            id: 2,
-            name: "Astral Projection",
-            position: "Backend developer",
-            createdUser: "Sabesh"
-        },
-        {
-            id: 3,
-            name: "Time Travel",
-            position: "UI UX Designer",
-            createdUser: "Kabilan"
-        }
+    const depts = [
+        "Information Technology",
+        "Computer Science Engineering",
+        "Electonics and Communication Engineering",
+        "Electrical and Electronic Engineering",
+        "Civil Engineering",
+        "Mechanical Engineering",
+        "Chemical Engineering",
+        "Biomedical Engineering"
     ]
+
+    const [redirect, setRedirect] = useState(false)
+    const [modal, setmodal] = useState(false)
+    const [projects, setProjects] = useState([])
+    const [users, setUsers] = useState([])
+    const [selectedUser, setSelectedUser] = useState(null)
+    const [selectedPos, setSelectedPos] = useState(null)
+    const [positions, setPositions] = useState([])
+
+    useEffect(() => {
+        axios.get(backEndUrl + '/api/projdeets')
+        .then(res => {
+            setProjects(res.data.projects)
+            setUsers(res.data.users)
+            setPositions(res.data.positions)
+        })
+        .catch(err => console.log(err.mess))
+        
+    }, [])
 
     const logout = () => {
         props.setIsLoggedIn(false)
         setRedirect(true)
+    }
+
+    const findUser = (ofProject) => {
+        if (users.length === 0)
+            return null
+        return  users.filter((u,i) => ofProject.ideaBy === u.id)[0]
+    }
+
+    const findPosition = (ofProject) => {
+        if (positions.length === 0)
+            return null
+        return  positions.filter((p,i) => ofProject.position === p.id)[0]
+    }
+
+    const toggler = () => {
+        setmodal(!modal)
+    }
+
+    const showDeets = (user, pos) => {
+        toggler()
+        setSelectedUser(user)
+        setSelectedPos(pos)
     }
 
     if(redirect)
@@ -39,8 +71,8 @@ export default function Dashboard(props) {
             <div className="container">
                 <br/><br/><br/><br/>
                 <div className="topbar">
-                    <h2 className="header">Dashboard - Projects</h2>
-                    <Button className="btn btn-danger logout" size="md"
+                    <h2 className="fleft">Dashboard - Projects</h2>
+                    <Button className="btn btn-danger fright" size="md"
                     onClick={logout}>Logout</Button>
                 </div>
                 <hr/>
@@ -52,17 +84,42 @@ export default function Dashboard(props) {
                                     <ToastHeader>
                                         Project Name: {p.name}
                                     </ToastHeader>
-                                    <ToastBody>
-                                        Position Needed: {p.position}
+                                    <ToastBody className="text-left">
+                                        Position Needed: {findPosition(p) === null ? null : findPosition(p).name}
+                                        <br/>
+                                        Posted by: {findUser(p) === null ? null : findUser(p).firstname}
                                     </ToastBody>
                                 </Toast>
+                                <Button className="btn btn-warning fright"
+                                onClick={() => showDeets(findUser(p), findPosition(p))}>Details</Button>
                             </div>
                         );
                     })
                 }
                 
                 <br/><br/><br/><br/><br/><br/>
-            </div> 
+            </div>
+            {
+                selectedUser ? 
+                <Modal isOpen={modal} toggle={toggler}>
+                    <ModalHeader toggle={toggler}>More Details</ModalHeader>
+                    <ModalBody>
+                        <h4>User Details</h4>
+                        <h6>First Name : {selectedUser.firstname}</h6>
+                        <h6>Last Name : {selectedUser.lastname}</h6>
+                        <h6>Department: {depts[selectedUser.dept - 1]}</h6>
+                        <h6>Role : {selectedUser.isstud ? "Student" : "Faculty"}</h6>
+                        <h6>Contact Email : {selectedUser.email}</h6>
+                        <br/>
+                        <h4>Position Details</h4>
+                        <h6>Position Needed: {selectedPos.name}</h6>
+                        <h6>Position Description: {selectedPos.desc}</h6>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={toggler}>Back</Button>{' '}
+                    </ModalFooter>
+                </Modal> : null
+            }
         </div>
     )
 }
