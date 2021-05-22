776f7d42f407b50a948fb4f3c3df5c81607041c4
Sabesh Bharathi <sabesh5601@gmail.com>
2021-05-22 19:17:02 +0530

    Added heroku

--- backend/procollab.py
+++ backend/procollab.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import User,Dept,Project,Position
 
-
+# Making commands available in shell
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Dept': Dept, 'Project': Project, 'Position': Position }
